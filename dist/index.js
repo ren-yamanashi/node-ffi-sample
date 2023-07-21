ec63812999a3226404a8342296e4a63443b494e1
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-07-21 14:42:21 +0900

    update .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ffi_napi_1 = require("ffi-napi");
-const LIBRARY_FILE = "lib/target/binding";
-const dll = (0, ffi_napi_1.Library)(LIBRARY_FILE, {
-    add: ["int", ["int", "int"]],
-});
-const ret = dll.add(1, 2);
-console.log(ret);
